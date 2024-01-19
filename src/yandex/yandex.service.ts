20cb7185d6ad40f745b2feb25245585c3ad6f2c3
Valery <valerkaogorodnikov@mail.ru>
2024-01-20 00:21:56 +0300

    add proxy

--- src/yandex/yandex.service.ts
+++ src/yandex/yandex.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios'
 import { BadGatewayException, Injectable } from '@nestjs/common'
-import { AxiosError } from 'axios'
+import axios, { AxiosError } from 'axios'
+import { HttpsProxyAgent } from 'https-proxy-agent'
 import { catchError, firstValueFrom } from 'rxjs'
 import { PrismaService } from 'src/prisma.service'
 import { YandexApiDto, YandexDto } from './dto/create-yandex.dto'
@@ -52,14 +53,42 @@ export class YandexService {
 	}
 
 	async generateResponseSyncChat() {
-		const url = 'https://api.openai.com/v1/chat/completions'
+		// const url = 'https://api.openai.com/v1/chat/completions'
+		// const data = {
+		// 	model: 'gpt-3.5-turbo',
+		// 	messages: [
+		// 		{
+		// 			role: 'user',
+		// 			content: 'Say this is a test!',
+		// 		},
+		// 		{
+		// 			role: 'assistant',
+		// 			content: 'This is a test!',
+		// 		},
+		// 		{
+		// 			role: 'user',
+		// 			content: 'Hello! How are you?',
+		// 		},
+		// 	],
+		// 	temperature: 0.7,
+		// }
 
-		const headers = {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${process.env.API_KEY_CHATGPT}`,
-		}
+		// const proxyUrl = 'http://nBM1a2:TLx66Y@185.88.98.71:8000'
 
-		const modifiedDto = {
+		// const proxyAgent = axios.create({
+		// 	httpAgent: new HttpsProxyAgent(proxyUrl),
+		// })
+
+		// try {
+		// 	const response = await proxyAgent.post(url, data, { headers })
+		// 	//console.log('response:', response.data)
+		// 	return response.data
+		// } catch (error) {
+		// 	//console.error('error while making axios POST request with proxy', error)
+		// 	throw error
+		// }
+
+		const data = {
 			model: 'gpt-3.5-turbo',
 			messages: [
 				{
@@ -78,16 +107,31 @@ export class YandexService {
 			temperature: 0.7,
 		}
 
-		const { data } = await firstValueFrom(
-			this.httpService.post<any>(url, modifiedDto, { headers }).pipe(
-				catchError((error: AxiosError) => {
-					throw new BadGatewayException(error)
-				})
-			)
-		)
+		const headers = {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer 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`,
+		}
 
-		return data
+		const httpsProxyUrl = 'http://aJYRJwCem:tJgZ2r8Ps@46.3.5.83:63410'
+
+		const httpsProxyAgent = axios.create({
+			httpsAgent: new HttpsProxyAgent(httpsProxyUrl),
+		})
+
+		try {
+			const response = await httpsProxyAgent.post(
+				'https://api.openai.com/v1/chat/completions',
+				data,
+				{ headers }
+			)
+			//console.log('response:', response.data)
+			return response.data
+		} catch (error) {
+			//console.error('error while making axios POST request with proxy', error)
+			throw error
+		}
 	}
+	//https://nBM1a2:TLx66Y@185.88.98.71:8000 - proxy
 
 	// Admin section
 
