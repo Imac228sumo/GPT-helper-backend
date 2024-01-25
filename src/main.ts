9e330d233080b28c9bfdf4fcbec64394ad0550f2
Valery <valerkaogorodnikov@mail.ru>
2024-01-25 17:08:39 +0300

    add session

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import * as passport from 'passport'
 import { AppModule } from './app.module'
-// import * as session from 'express-session'
-// import * as passport from 'passport'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 
-	// app.use(
-	// 	session({
-	// 		secret: 'keyword',
-	// 		resave: false,
-	// 		saveUninitialized: false,
-	// 	})
-	// )
-	// app.use(passport.initialize())
-	// app.use(passport.session())
+	app.use(
+		session({
+			secret: 'keyword',
+			resave: false,
+			saveUninitialized: false,
+		})
+	)
+	app.use(passport.initialize())
+	app.use(passport.session())
 	app.use(cookieParser())
 
 	app.enableCors({
